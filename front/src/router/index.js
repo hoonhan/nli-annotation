54ec85fe3b1519999b996a74ef05e1f87c8f5824
righthan <hanuright@gmail.com>
2020-07-14 17:12:13 +0900

    Front base structure - Change the file names comprehensively

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Instruction from '../views/Instruction.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Instruction',
+    component: Instruction
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/annotation',
+    name: 'Annotation',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Annotation.vue')
   }
 ]
 
---
+++ front/src/views/Annotation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <premise msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Premise from '@/components/Premise.vue'
+
+export default {
+  name: 'Annotation',
+  components: {
+    Premise
+  }
+}
+</script>
