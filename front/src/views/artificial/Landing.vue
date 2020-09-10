49055c8a2b5d7715dad4d26fa69f9173283e8aa0
righthan <hanuright@gmail.com>
2020-09-10 17:31:28 +0900

    multiart survey link updt

--- front/src/views/artificial/Landing.vue
+++ front/src/views/artificial/Landing.vue
@@ -85,8 +85,6 @@ export default {
       self.$refs.form.validate()
       self.$store.commit('set_mturk_id', self.turk_id.trim())
       self.$helpers.server_call(self, function(self, res){
-        console.log(res.data.user_type)
-        console.log(self.$store.state.user_type)
         // if (res.data.user_type != self.$store.state.user_type) {
         //   throw new Error('Abnormal access to the webpage detected. You might have joined our website for another task before.')
         // }
